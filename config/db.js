ac4ff7872285dd0d85352a34620ea327fdd85ed1
Maruf Murshed <marufmurshed9255@gmail.com>
2024-05-11 22:18:27 +0600

    virtually db connected

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const mySqlPool = mysql.createPool({
-    host: 'localhost',
+    host: 'srv1038.hstgr.io',
     user: 'u793304885_admin',
     password: '|I2+Q&3SB>cn',
     database: 'u793304885_qamla_db'
