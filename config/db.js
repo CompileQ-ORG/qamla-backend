41ebe70c9853fb8c7c35784e16cf8e8d80109d59
Maruf Murshed <marufmurshed9255@gmail.com>
2024-05-11 22:01:00 +0600

    database connection added

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const mySqlPool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'qamla_db'
+    user: 'u793304885_admin',
+    password: '|I2+Q&3SB>cn',
+    database: 'u793304885_qamla_db'
 })
 
 module.exports = mySqlPool;
